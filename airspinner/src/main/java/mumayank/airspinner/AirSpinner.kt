2b9c24a0cf756b8711bd5cb21c9f998c56aa8890
mu <mumayank@gmail.com>
2019-01-03 10:44:03 +0530

    update

--- airspinner/src/main/java/mumayank/airspinner/AirSpinner.kt
+++ airspinner/src/main/java/mumayank/airspinner/AirSpinner.kt
@@ -9,49 +9,51 @@ import android.view.ViewGroup
 import mumayank.airspinner.lib.R
 import mumayank.com.airdialog.AirDialog
 
-class AirSpinner {
+class AirSpinner(
+    activity: Activity,
+    parentLayoutViewGroup: ViewGroup,
+    title: String,
+    charSequenceArray: Array<CharSequence>,
+    callback: Callback
+) {
 
     interface Callback {
         fun onSelected(selectedOptionIndex: Int)
     }
 
-    companion object {
+    private var textInputLayout: TextInputLayout? = null
+    private var textInputEditText: TextInputEditText? = null
 
-        fun init(
-            activity: Activity,
-            parentLayoutViewGroup: ViewGroup,
-            title: String,
-            charSequenceArray: Array<CharSequence>,
-            callback: Callback
-        ) {
-            val airOutlineSpinnerView = LayoutInflater.from(activity).inflate(R.layout.air_outline_spinner, parentLayoutViewGroup, true)
+    init {
+        val airOutlineSpinnerView = LayoutInflater.from(activity).inflate(R.layout.air_outline_spinner, parentLayoutViewGroup, true)
 
-            val textInputLayout = airOutlineSpinnerView.findViewById<TextInputLayout>(R.id.textInputLayout)
-            textInputLayout.hint = title
+        textInputLayout = airOutlineSpinnerView.findViewById(R.id.textInputLayout)
+        textInputLayout?.hint = title
 
-            val textInputEditText = airOutlineSpinnerView.findViewById(R.id.textInputEditText) as TextInputEditText
-            textInputEditText.setText(charSequenceArray.get(0).toString())
-            textInputEditText.setOnClickListener {
-                val currentlySelectedIndex = charSequenceArray.indexOf(textInputEditText.text.toString())
-                AirDialog(activity, title).showOptions(
-                    charSequenceArray,
-                    onOptionsCallback = object: AirDialog.ShowOptionsCallback {
-                        override fun onCancelled() {
-                            // do nothing
-                        }
+        textInputEditText = airOutlineSpinnerView.findViewById(R.id.textInputEditText)
+        textInputEditText?.setText(charSequenceArray.get(0).toString())
+        textInputEditText?.setOnClickListener {
+            val currentlySelectedIndex = charSequenceArray.indexOf(textInputEditText?.text.toString())
+            AirDialog(activity, title).showOptions(
+                charSequenceArray,
+                onOptionsCallback = object: AirDialog.ShowOptionsCallback {
+                    override fun onCancelled() {
+                        // do nothing
+                    }
 
-                        override fun onOptionSelected(selectedOptionIndex: Int) {
-                            textInputEditText.setText(charSequenceArray.get(selectedOptionIndex).toString())
-                            callback.onSelected(selectedOptionIndex)
-                        }
+                    override fun onOptionSelected(selectedOptionIndex: Int) {
+                        textInputEditText?.setText(charSequenceArray.get(selectedOptionIndex).toString())
+                        callback.onSelected(selectedOptionIndex)
+                    }
 
-                    },
-                    defaultSelectedOptionIndex = currentlySelectedIndex
-                )
+                },
+                defaultSelectedOptionIndex = currentlySelectedIndex
+            )
 
-            }
         }
-
     }
 
+    fun getText(): String {
+        return textInputEditText?.text.toString()
+    }
 }
